74f8554694cb57d4a5c4bae351157b07bade7cf1
Aryan0Jain <aryanajjain@gmail.com>
2023-12-15 18:49:35 +0530

    CSS Added

--- public/js/menu.js
+++ public/js/menu.js
@@ -9,4 +9,36 @@ document.addEventListener("DOMContentLoaded", () => {
 		.addEventListener("click", () => {
 			document.querySelector(".ham-background").style.display = "none";
 		});
+	let currentActiveTabSuccess = 1;
+	let successTabs = [...document.querySelectorAll(".success .tab")];
+	let successButtons = [...document.querySelectorAll(".success button")];
+	successButtons.forEach((btn, index) => {
+		btn.addEventListener("click", () => {
+			successTabs[currentActiveTabSuccess - 1].id = "";
+			successTabs[index].id = "tab-active";
+			successButtons[currentActiveTabSuccess - 1]
+				.querySelector("path")
+				.classList.remove("active-svg");
+			successButtons[index]
+				.querySelector("path")
+				.classList.add("active-svg");
+			currentActiveTabSuccess = index + 1;
+		});
+	});
+	let currentActiveTabProgram = 1;
+	let programTabs = [...document.querySelectorAll(".program-2 .tab")];
+	let programButtons = [...document.querySelectorAll(".program-2 button")];
+	programButtons.forEach((btn, index) => {
+		btn.addEventListener("click", () => {
+			programTabs[currentActiveTabProgram - 1].id = "";
+			programTabs[index].id = "tab-active";
+			programButtons[currentActiveTabProgram - 1]
+				.querySelector("path")
+				.classList.remove("active-svg");
+			programButtons[index]
+				.querySelector("path")
+				.classList.add("active-svg");
+			currentActiveTabProgram = index + 1;
+		});
+	});
 });
