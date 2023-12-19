55fb3dcb346b3f07c4f713cda7851a4fea3ceb81
Aryan0Jain <aryanajjain@gmail.com>
2023-12-19 15:30:58 +0530

    Profile Page Designed

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,50 +1,55 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const sections = document.querySelectorAll(".section");
-  const header = document.querySelector("header"); 
-  const colors = ["#ffffff","#3498db", "#e74c3c", "#2ecc71","#ffffff"]; // Different background colors for each section
-  const checkpoint = 100; // Adjust this value as needed
+// const updateButton = document.getElementById("update-profile-button");
+// updateButton.style.display = "none";
+function editField(fieldName) {
+	const fieldElement = document.getElementById(`${fieldName}-input`);
+	const updateButton = document.getElementById("update-profile-button");
+	if (fieldElement) {
+		const isHidden =
+			fieldElement.style.display === "none" ||
+			fieldElement.style.display === "";
 
-  function changeSectionTheme() {
-    const scrollPosition = window.scrollY;
+		fieldElement.style.display = isHidden ? "block" : "none";
+		if (fieldElement.style.display === "block") {
+			fieldElement.setAttribute("required", "true");
+			console.log(fieldElement);
+		} else {
+			fieldElement.removeAttribute("required");
+			console.log(fieldElement);
+		}
+		// Show the "Update Profile" button only when an input field is displayed
+		updateButton.style.display = isHidden ? "block" : "none";
+	}
+}
 
-    for (let i = 0; i < sections.length; i++) {
-      const sectionTop = sections[i].offsetTop;
-      if (scrollPosition >= sectionTop - checkpoint) {
-        document.body.style.transition = "background-color 0.5s"; // Add transition
-        document.body.style.backgroundColor = colors[i];
-        header.style.transition = "background-color 0.5s"; 
-        header.style.backgroundColor = colors[i];
-      }
-    }
-  }
-
-  // Initial theme based on the first section
-  changeSectionTheme();
-
-  // Update the theme as the user scrolls
-  window.addEventListener("scroll", changeSectionTheme);
+const form = document.querySelector(".edit-form");
+const detailTitle = document.querySelector(".edit-form .detail-title");
+const inputInfo = document.querySelector(".edit-form .input-info");
+const input = document.querySelector(".edit-form input");
+const formError = document.querySelector(".edit-form .form-error");
+const submitBtn = document.querySelector(".edit-form .update-detail");
+const cancelBtn = document.querySelector(".edit-form .cancel-detail");
+const editObj = {
+	form,
+	title: detailTitle,
+	inputInfo,
+	input,
+	error: formError,
+	submitBtn,
+	cancelBtn,
+};
+cancelBtn.addEventListener("click", () => {
+	form.style.display = "none";
 });
 
+console.dir(editObj);
+function updateProfile(detail) {
+	editObj.form.style.display = "flex";
+	editObj.title.innerHTML = "Update your " + detail;
+	editObj.input.type =
+		detail == "Picture" ? "file" : detail == "Number" ? "number" : "text";
+	editObj.error.innerHTML = "";
 
-const updateButton = document.getElementById('update-profile-button');
-updateButton.style.display="none"
-function editField(fieldName) {
-  const fieldElement = document.getElementById(`${fieldName}-input`);
-  const updateButton = document.getElementById('update-profile-button');
-  if (fieldElement) {
-    const isHidden = fieldElement.style.display === 'none' || fieldElement.style.display === '';
-
-    fieldElement.style.display = isHidden ? 'block' : 'none';
-    if(fieldElement.style.display === "block"){
-        fieldElement.setAttribute("required","true")
-        console.log(fieldElement)
-    }
-    else{
-     fieldElement.removeAttribute("required");
-     console.log(fieldElement)
-    }
-    // Show the "Update Profile" button only when an input field is displayed
-    updateButton.style.display = isHidden ? 'block' : 'none';
-  }
+	////////////////////////////////////////////////
+	////Write Code here to update database
+	///////////////////////////////////////////////
 }
- 
