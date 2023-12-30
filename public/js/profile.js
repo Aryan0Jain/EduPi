07f4c294844f744bacb655638b79e8f91fc02392
Adfar Rashid <81241885+adfar-136@users.noreply.github.com>
2023-12-30 11:39:58 +0530

    Update profile.js

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,25 +1,6 @@
 // const updateButton = document.getElementById("update-profile-button");
 // updateButton.style.display = "none";
-function editField(fieldName) {
-	const fieldElement = document.getElementById(`${fieldName}-input`);
-	const updateButton = document.getElementById("update-profile-button");
-	if (fieldElement) {
-		const isHidden =
-			fieldElement.style.display === "none" ||
-			fieldElement.style.display === "";
 
-		fieldElement.style.display = isHidden ? "block" : "none";
-		if (fieldElement.style.display === "block") {
-			fieldElement.setAttribute("required", "true");
-			console.log(fieldElement);
-		} else {
-			fieldElement.removeAttribute("required");
-			console.log(fieldElement);
-		}
-		// Show the "Update Profile" button only when an input field is displayed
-		updateButton.style.display = isHidden ? "block" : "none";
-	}
-}
 
 const form = document.querySelector(".edit-form");
 const detailTitle = document.querySelector(".edit-form .detail-title");
@@ -48,7 +29,9 @@ function updateProfile(detail) {
 	editObj.input.type =
 		detail == "Picture" ? "file" : detail == "Number" ? "number" : "text";
 	editObj.error.innerHTML = "";
-
+   const inputUpdate = document.getElementById("inputUpdate")
+   console.log(detail);
+   inputUpdate.setAttribute('name', detail);
 	////////////////////////////////////////////////
 	////Write Code here to update database
 	///////////////////////////////////////////////
